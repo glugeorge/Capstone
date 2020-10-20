27f2fc4295fc7aa2b79972272170864c6f505b75
glugeorge <glugeorge@gmail.com>
2020-10-20 15:14:47 -0400

    Added current setting function for dc power supply

--- functions/dc.py
+++ functions/dc.py
@@ -8,6 +8,9 @@ def channel_on_off(device,channel,state):
 def set_voltage_level(device,channel,voltage):
     device.write("VOLT {}, (@{})".format(voltage,channel))
 
+def set_current_level(device,channel,current)
+    dc_ps.write("CURR {},(@{})".format(current,channel))
+
 ## Below is test code ##
 if __name__ == "__main__":
     dc = init_device("USB0::0x2A8D::0x1002::MY59001637::INSTR")
