dc4e69eac59755b96bcc6f8903b42ca0766a2b8f
glugeorge <glugeorge@gmail.com>
2020-10-20 14:12:28 -0400

    Added basic DC Supply functions

--- functions/dc.py
+++ functions/dc.py
@@ -1,3 +1,15 @@
 from import_libraries import *
 from common_functions import *
 from global_variables import *
+
+def channel_on_off(device,channel,state):
+    device.write("OUTPut {}, (@{})".format(state,channel))
+# TODO: Add error handling
+
+def set_voltage_level(device,channel,voltage):
+    device.write("VOLT {}, (@{})".format(voltage,channel))
+
+## Below is test code ##
+if __name__ == "__main__":
+    dc = init_device("USB0::0x2A8D::0x1002::MY59001637::INSTR")
+    set_voltage_level(dc,1,0.5)
