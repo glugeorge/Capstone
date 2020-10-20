6a9d6c0862ca300d1af88f7f13c7e0f8cb1300eb
glugeorge <glugeorge@gmail.com>
2020-10-20 14:37:22 -0400

    Added basic DAQ functions

--- functions/daq.py
+++ functions/daq.py
@@ -1,3 +1,17 @@
-from import_libraries import *
 from common_functions import *
 from global_variables import *
+
+def set_sample_rate(device,channel,rate):
+    device.write("SAMPle3:RATE {},(@{})".format(rate,channel))
+
+def set_sample_count(device,channel,count):
+    device.write("SAMPle3:COUNt {},(@{})".format(count,channel))
+
+def take_measurement(device,channel,rate,count):
+    set_sample_rate(device,channel,rate)
+    set_sample_count(device,channel,count)
+    device.write("INITiate3 (@{})".format(channel))
+    device.timeout = int(count)/int(rate)
+# May need to do some conversion of sorts from their rate format to integer
+    data = device.query("FETCh3? (@{})".format(channel))
+    return data
--- functions/dc.py
+++ functions/dc.py
@@ -1,4 +1,3 @@
-from import_libraries import *
 from common_functions import *
 from global_variables import *
 
