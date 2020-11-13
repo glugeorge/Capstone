ab9cad0df09506c068239873b9a01e2e4e722ca5
glugeorge <glugeorge@gmail.com>
2020-11-12 20:35:33 -0500

    Update plotter.py

--- functions/plotter.py
+++ functions/plotter.py
@@ -2,7 +2,7 @@ from common_functions import *
 from global_variables import *
 
 # Takes in live data (from file), plots it
-def live_plot(refresh_rate=1000, data_file, scroll=False): #default is 1 sample per second
+def live_plot(data_file, scroll=True ,refresh_rate=1000): #default is 1 sample per second
     # Create figure for plotting
     fig = plt.figure()
     ax = fig.add_subplot(1, 1, 1)
@@ -36,3 +36,6 @@ def live_plot(refresh_rate=1000, data_file, scroll=False): #default is 1 sample 
     
     ani = animation.FuncAnimation(fig, animate, interval=refresh_rate)
     plt.show()
+
+if __name__ == "__main__":
+    live_plot('test.txt')
