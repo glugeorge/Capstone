46d6077a9c59d328679a6a7b0a58c5e58fd621e2
glugeorge <glugeorge@gmail.com>
2020-11-12 19:47:22 -0500

    Update plotter.py

--- functions/plotter.py
+++ functions/plotter.py
@@ -1,18 +1,20 @@
 from common_functions import *
 from global_variables import *
 
-# animate function
-def animate(i,ax,x,y):
-    ax.clear()
-    ax.plot(x,y)
-
-
 # Takes in live data (from file), plots it
-def live_plot(refresh_rate, data_file):
+def live_plot(refresh_rate=1000, data_file, scroll=False): #default is 1 sample per second
     # Create figure for plotting
     fig = plt.figure()
     ax = fig.add_subplot(1, 1, 1)
-    # Parse data_file for x, y 
     
-    ani = animation.FuncAnimation(fig, animate, fargs=(ax, x, y), interval=refresh_rate)
+    # animate function
+    def animate(i):
+        # Parse data file for x and y
+        
+        if scroll and len(x)> 20: # Arbitrary window length, tbd
+            x, y = x[-20:], y[-20:]
+        ax.clear()
+        ax.plot(x,y)
+    
+    ani = animation.FuncAnimation(fig, animate, interval=refresh_rate)
     plt.show()
