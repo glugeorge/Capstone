e5e39ef17df8c8a1eabab2a3f720e95c78397370
evandalacu <73136477+evandalacu@users.noreply.github.com>
2020-11-24 23:19:42 -0500

    Update plotter_syncronous.py

--- functions/plotter_syncronous.py
+++ functions/plotter_syncronous.py
@@ -7,29 +7,36 @@ def take_measurement(type, t_0, device, channel):
         return random.random(), random.random()
     if type == "random_vs_time":
         t = time.time() - t_0
+        y = random.random()
+        return t, y
+    if type == "voltage_vs_time":
+        t = time.time() - t_0
         volt_ascii = daq.take_measurement(device, channel)
         voltage = np.mean([float(s) for s in volt_ascii.split(',')])
         return t, voltage
 
-def choose_titles(type):
+def set_chart_titles(type):
     if type == "random":
         return "random values", "random x", "random y"
     if type == "random_vs_time":
         return "random values vs time", "time", "random y"
+    if type == "voltage_vs_time":
+        return "voltage vs time", "time", "voltage"
+
+def initiate_measurement(type):
+    if type == "voltage_vs_time":
+        dc_ps_dev, daq_dev, dmm_dev = init_devices(['USB0::0x2A8D::0x1002::MY59001637::INSTR',
+                                              'USB0::0x2A8D::0x5101::MY58002845::0::INSTR',
+                                              'USB0::0x2A8D::0x1301::MY59033786::INSTR'])
+        dc.set_voltage_level(dc_ps_dev, 1, 1)
+        dc.set_current_level(dc_ps_dev, 1, 0.2)
+        daq.initialize_device(daq_dev, 102, rate=800E3, voltage=18)
+        return daq_dev
 
 # Takes in live data (from file), plots it
 def live_plot(filename, type, scroll=True, refresh_rate=1000): #default is 1 sample per second
-    
-    dc_ps_dev, daq_dev, dmm_dev = init_devices(['USB0::0x2A8D::0x1002::MY59001637::INSTR', 
-                                      'USB0::0x2A8D::0x5101::MY58002845::0::INSTR',
-                                      'USB0::0x2A8D::0x1301::MY59033786::INSTR'])
-    
-    dc.set_voltage_level(dc_ps_dev, 1, 1)
-    dc.set_current_level(dc_ps_dev, 1, 0.2)
-    daq.initialize_device(daq_dev, 102, rate=800E3, voltage=18)
-
-
-    chart_title, chart_x, chart_y = choose_titles(type)
+    daq_dev = initiate_measurement(type)
+    chart_title, chart_x, chart_y = set_chart_titles(type)
     data_file = initiate_file(filename, chart_title, chart_x, chart_y)
     absolute_time = time.time()
     # Create figure for plotting
@@ -47,6 +54,11 @@ def live_plot(filename, type, scroll=True, refresh_rate=1000): #default is 1 sam
     def animate(i):
         item1, item2 = take_measurement(type, absolute_time, daq_dev, 104)
         save_to_file(data_file, item1, item2)
+        x.append(item1)
+        y.append(item2)
+
+        """
+        # Slows down code, consider replacing with append statement above
         # Parse data file for x and y
         f = open(data_file, "r")
         lines = f.readlines()
@@ -55,6 +67,7 @@ def live_plot(filename, type, scroll=True, refresh_rate=1000): #default is 1 sam
             for line in lines[len(x)+2:]:
                 x.append(float(line.split(",")[0]))
                 y.append(float(line.split(",")[1]))
+        """
 
         if scroll and len(x)> 20: # Arbitrary window length, tbd
             x_plot, y_plot = x[-20:], y[-20:]
